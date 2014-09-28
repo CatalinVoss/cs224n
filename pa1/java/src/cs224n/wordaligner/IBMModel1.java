a26bb1e20abeaed619470df4fbb85f04b157439b
Catalin Voss <catalin@cs.stanford.edu>
2014-09-28 16:01:34 -0700

    Add stubs for model implementation

---
+++ pa1/java/src/cs224n/wordaligner/IBMModel1.java
@@ -0,0 +1,23 @@
+package cs224n.wordaligner;
+
+import java.util.List;
+
+/**
+ * IBM Model 1 implementation.
+ * 
+ * @author John Miller
+ * @author Catalin Voss
+ */
+public class IBMModel1 implements WordAligner {
+  @Override
+  public Alignment align(SentencePair sentencePair) {
+	// TODO: implement	
+  	return new Alignment();
+  }
+
+  @Override
+  public void train(List<SentencePair> trainingData) {
+  	// TODO: implement
+  }
+
+}
---
+++ pa1/java/src/cs224n/wordaligner/PMIModel.java
@@ -0,0 +1,22 @@
+package cs224n.wordaligner;
+
+import java.util.List;
+
+/**
+ * PMI Model implementation.
+ * 
+ * @author John Miller
+ * @author Catalin Voss
+ */
+public class PMIModel implements WordAligner {
+  @Override
+  public Alignment align(SentencePair sentencePair) {
+	// TODO: implement	
+  	return new Alignment();
+  }
+
+  @Override
+  public void train(List<SentencePair> trainingData) {
+  	// TODO: implement
+  }
+}
