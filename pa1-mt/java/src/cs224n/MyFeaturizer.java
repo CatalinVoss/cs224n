f57acef0e6e38b8292ac4d6ef95423d00c124cba
Catalin Voss <catalin@stanford.edu>
2014-10-09 17:18:39 -0700

    Clean up mt directory

--- pa1-mt/java/src/cs224n/MyFeaturizer.java
+++ pa1-mt/java/src/cs224n/MyFeaturizer.java
@@ -23,44 +23,43 @@ public class MyFeaturizer implements RuleFeaturizer<IString, String> {
       if (phrase.contains(sub))
         result++;
     }
-    return result+1; // "sort of laplace smoothing"
+    return result+1; // Make sure we don't divide by zero by this sort of laplace smoothing
   }
 
   @Override
   public void initialize() {
-    // Do any setup here.
+    // Nothing to do
   }
 
   @Override
   public List<FeatureValue<String>> ruleFeaturize(Featurizable<IString, String> f) {  
-    // TODO: Return a list of features for the rule. Replace these lines
-    // with your own feature.
     List<FeatureValue<String>> features = Generics.newLinkedList();
-    // features.add(new FeatureValue<String>( String.format("TGTD:%d", f.targetPhrase.size()), 1.0));
-    // features.add(new FeatureValue<String>( String.format("SCTD:%d", f.sourcePhrase.size()), 1.0));
+
+    // Final features:
+    
+    // Combination of target and source phrase size
     features.add(new FeatureValue<String>( String.format("COMB:%d_%d", f.targetPhrase.size(),f.sourcePhrase.size()), 1.0));
- 
+
+    // Punctuation ratio
+    features.add(new FeatureValue<String>("PUNC_RATIO", (double)countHits(f.sourcePhrase.toString(), punct)/countHits(f.targetPhrase.toString(), punct) ));
+
+    //// Various previous experiments (see writeup for details)
+    ////////////////////////////////////////////////////////////
     // int d = Math.abs(f.sourcePhrase.size()-f.targetPhrase.size());
     // if (d < 3)
     //   features.add(new FeatureValue<String>("DONUT",1.0)); // f.sourcePhrase.size() > 2
     // features.add(new FeatureValue<String>(String.format("DIST:%d", d),1.0)); // f.sourcePhrase.size() > 2
-    
     // if (f.targetPhrase.toString().contains(",")&&f.sourcePhrase.toString().contains(","))
     //   features.add(new FeatureValue<String>("GOODCOMMAS",1.0));
     // else if ((f.targetPhrase.toString().contains(",")&&!f.sourcePhrase.toString().contains(","))||(!f.targetPhrase.toString().contains(",")&&f.sourcePhrase.toString().contains(",")))
     //   features.add(new FeatureValue<String>("BADCOMMAS",1.0));
-
     // if (f.targetPhrase.toString().contains(".")&&f.sourcePhrase.toString().contains("."))
     //   features.add(new FeatureValue<String>("GOODPERIOD",1.0));
     // else if ((f.targetPhrase.toString().contains(".")&&!f.sourcePhrase.toString().contains("."))||(!f.targetPhrase.toString().contains(".")&&f.sourcePhrase.toString().contains(".")))
     //   features.add(new FeatureValue<String>("BADPERIOD",1.0));
-
-    features.add(new FeatureValue<String>("PUNC_RATIO", (double)countHits(f.sourcePhrase.toString(), punct)/countHits(f.targetPhrase.toString(), punct) ));
-
     // if (countHits(f.targetPhrase.toString(), numbers) == countHits(f.sourcePhrase.toString(), numbers)) {
     //   features.add(new FeatureValue<String>("MATCHINGNUMS",1.0));
     // }
-
     // features.add(new FeatureValue<String>("JUNK-RATIO", (double)(countHits(f.targetPhrase.toString(), junkWords))/countHits(f.sourcePhrase.toString(), junkWords) ));
 
     return features;
